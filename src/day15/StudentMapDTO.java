c45395eb8cb589be55334181d2fe3c11e5761702
kwucslkm <kwucsani@naver.com>
2023-03-15 09:16:29 +0900

    map second commit 230315

--- src/day15/StudentMapDTO.java
+++ src/day15/StudentMapDTO.java
@@ -2,14 +2,52 @@ package day15;
 
 public class StudentMapDTO {
 
+	private Long id;
+	private String studentNumber;
+	private String studentName;
+	private String studentMajor;
+	private String studentMobile;
 
-	public int id;
-	public String studentNumber;
-	public String studentName;
-	public String studentMajor;
-	public String studentMobile;
+	public Long getId() {
+		return id;
+	}
 
-	
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getStudentNumber() {
+		return studentNumber;
+	}
+
+	public void setStudentNumber(String studentNumber) {
+		this.studentNumber = studentNumber;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public String getStudentMajor() {
+		return studentMajor;
+	}
+
+	public void setStudentMajor(String studentMajor) {
+		this.studentMajor = studentMajor;
+	}
+
+	public String getStudentMobile() {
+		return studentMobile;
+	}
+
+	public void setStudentMobile(String studentMobile) {
+		this.studentMobile = studentMobile;
+	}
+
 	@Override
 	public String toString() {
 		String str = id + "\t" + studentNumber + "\t" + studentName + "\t" + studentMajor + "\t" + studentMobile;
--- src/day15/StudentMapMain.java
+++ src/day15/StudentMapMain.java
@@ -6,44 +6,58 @@ import java.util.Map;
 public class StudentMapMain {
 
 	public static void main(String[] args) {
-		// 
-		Map<Integer, StudentMapDTO> studentmap = new HashMap<>();
-		StudentMapDTO studentmapDTO =new  StudentMapDTO();
-		studentmapDTO.id=1;
-		studentmapDTO.studentNumber="1111";
-		studentmapDTO.studentName="김자바";
-		studentmapDTO.studentMajor="컴퓨터공학";
-		studentmapDTO.studentMobile="010-1111-1111";
-		
-		studentmap.put(1, studentmapDTO);
-		StudentMapDTO studentmapDTO1 =new  StudentMapDTO();
-		studentmapDTO1.id = 2;
-		studentmapDTO1.studentNumber = "2222";
-		studentmapDTO1.studentName = "이자바";
-		studentmapDTO1.studentMajor = "경영학";
-		studentmapDTO1.studentMobile = "010-2222-2222";
-		studentmap.put(2, studentmapDTO1);
-//		System.out.println(studentmap.get(2));
-//		StudentMapDTO studentmapDTO3 = new StudentMapDTO();
-		StudentMapDTO studentmapDTO2 =new  StudentMapDTO();
-		studentmapDTO2.id = 3;
-		studentmapDTO2.studentNumber = "3333";
-		studentmapDTO2.studentName = "박자바";
-		studentmapDTO2.studentMajor = "전자공학";
-		studentmapDTO2.studentMobile = "010-3333-3333";
-		
-		studentmap.put(3,studentmapDTO2);
-//		System.out.println(studentmap.get(3));
+		//
+		Map<Long, StudentMapDTO> studentmap = new HashMap<>();
+		StudentMapDTO studentmapDTO = new StudentMapDTO();
+		studentmapDTO.setId(1L);
+		studentmapDTO.setStudentNumber("1111");
+		studentmapDTO.setStudentName("김자바");
+		studentmapDTO.setStudentMajor("컴퓨터공학");
+		studentmapDTO.setStudentMobile("010-1111-1111");
+		studentmap.put(studentmapDTO.getId(), studentmapDTO);
+
+		StudentMapDTO studentmapDTO1 = new StudentMapDTO();
+		studentmapDTO1.setId(2L);
+		studentmapDTO1.setStudentNumber("2222");
+		studentmapDTO1.setStudentName("이자바");
+		studentmapDTO1.setStudentMajor("경영학");
+		studentmapDTO1.setStudentMobile("010-1111-1111");
+		studentmap.put(studentmapDTO1.getId(), studentmapDTO1);
+
+		StudentMapDTO studentmapDTO2 = new StudentMapDTO();
+		studentmapDTO2.setId(3L);
+		studentmapDTO2.setStudentNumber("3333");
+		studentmapDTO2.setStudentName("박자바");
+		studentmapDTO2.setStudentMajor("전자공학");
+		studentmapDTO2.setStudentMobile("010-1111-1111");
+		studentmap.put(studentmapDTO2.getId(), studentmapDTO2);
+
 		System.out.println("-------------------------------------------------------");
 		System.out.println("번호\t학번\t이름\t전공\t전화번호");
 		System.out.println("-------------------------------------------------------");
-		for (Integer st : studentmap.keySet()) {
-			System.out.println(st);
+		for (Long st : studentmap.keySet()) {
+//			System.out.println(st);
 			System.out.println(studentmap.get(st));
-//			System.out.println(studentmap.get(st).toString());
+			System.out.println(studentmap.get(st).getStudentName());
 
 		}
 
 	}
 
 }
+//StudentMapDTO studentmapDTO1 =new  StudentMapDTO();
+//studentmapDTO1.id = 2;
+//studentmapDTO1.studentNumber = "2222";
+//studentmapDTO1.studentName = "이자바";
+//studentmapDTO1.studentMajor = "전자공학";
+//studentmapDTO1.studentMobile = "010-2222-2222";
+//
+////System.out.println(studentmap.get(2));
+////StudentMapDTO studentmapDTO3 = new StudentMapDTO();
+//StudentMapDTO studentmapDTO2 =new  StudentMapDTO();
+//studentmapDTO2.id = 3;
+//studentmapDTO2.studentNumber = "3333";
+//studentmapDTO2.studentName = "박자바";
+//studentmapDTO2.studentMajor = "전자공학";
+//studentmapDTO2.studentMobile = "010-3333-3333";
+//
