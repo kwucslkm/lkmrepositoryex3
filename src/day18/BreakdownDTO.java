dd9bc21d6eb1fd208092bd67af881aa87f49fb91
kwucslkm <kwucsani@naver.com>
2023-03-17 19:10:40 +0900

    client 7

--- src/day18/BreakdownDTO.java
+++ src/day18/BreakdownDTO.java
@@ -9,20 +9,18 @@ import java.time.format.DateTimeFormatter;
  */
 public class BreakdownDTO {
 	private final static DateTimeFormatter DTF = DateTimeFormatter.ofPattern("yy/MM/dd hh:mm:ss");
-	private static Integer number= 100;
+	private static Integer number = 100;
 	private String dealNo;
 	private String account;
 	private String division;
 	private long dealMoney;
 	private long totalMoney;
 	private String dealDate;
-	
 
-	
 	public BreakdownDTO() {
 		this.dealDate = DTF.format(LocalDateTime.now());
-		
-		this.dealNo= "B"+number++; 
+
+		this.dealNo = "B" + number++;
 	}
 	public String getDealDate() {
 		return dealDate;
@@ -32,8 +30,6 @@ public class BreakdownDTO {
 		this.dealDate = dealDate;
 	}
 
-	
-
 	public String getDealNo() {
 		return dealNo;
 	}
@@ -42,8 +38,6 @@ public class BreakdownDTO {
 		this.dealNo = dealNo;
 	}
 
-
-	
 	public String getAccount() {
 		return account;
 	}
@@ -68,11 +62,10 @@ public class BreakdownDTO {
 	public void setTotalMoney(long totalMoney) {
 		this.totalMoney = totalMoney;
 	}
-	
+
 	@Override
 	public String toString() {
-		String str = account+"\t"+division+"\t"+dealMoney+
-				"\t"+totalMoney+"\t"+dealDate;
+		String str = account + "\t" + division + "\t" + dealMoney + "\t" + totalMoney + "\t" + dealDate;
 		return str;
 	}
 	public static Integer getNumber() {
@@ -81,10 +74,5 @@ public class BreakdownDTO {
 	public static void setNumber(Integer number) {
 		BreakdownDTO.number = number;
 	}
-	
+
 }
-
-
-
-
-
